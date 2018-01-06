f46537b6f656b66af6565ba7f58e6dba93f7bf29
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-01-06 15:01:38 +0200

    Avoiding eval

--- test/test.js
+++ test/test.js
@@ -12,126 +12,130 @@ function show(x) {
   }
 }
 
-function testEq(exprIn, expect) {
-  const expr = exprIn.replace(/[ \n]+/g, ' ')
+function testEq(expect, thunk) {
+  const expr = thunk
+    .toString()
+    .replace(/[ \n]+/g, ' ')
+    .replace(/^\s*function\s*\(\s*\)\s*{\s*(return\s*)?/, '')
+    .replace(/\s*;?\s*}\s*$/, '')
 
   it(`${expr} => ${show(expect)}`, () => {
-    const actual = eval(`() => ${expr}`)(PP, I)
+    const actual = thunk()
     if (!I.acyclicEqualsU(actual, expect))
       throw new Error(`Expected: ${show(expect)}, actual: ${show(actual)}`)
   })
 }
 
 describe('prettier-printer', () => {
-  testEq(`I.seq('foo', PP.render(10))`, 'foo')
+  testEq('foo', () => I.seq('foo', PP.render(10)))
 
-  testEq(`I.seq('bar', PP.prepend('foo'), PP.render(10))`, 'foobar')
+  testEq('foobar', () => I.seq('bar', PP.prepend('foo'), PP.render(10)))
 
-  testEq(`I.seq('foo', PP.append('bar'), PP.render(10))`, 'foobar')
+  testEq('foobar', () => I.seq('foo', PP.append('bar'), PP.render(10)))
 
-  testEq(`I.seq(['foo', '', 'bar'], PP.render(10))`, 'foobar')
+  testEq('foobar', () => I.seq(['foo', '', 'bar'], PP.render(10)))
 
-  testEq(`I.seq([], PP.render(10))`, '')
+  testEq('', () => I.seq([], PP.render(10)))
 
-  testEq(`I.seq(PP.intersperse('-', ['foo', 'bar']), PP.render(10))`, 'foo-bar')
-
-  testEq(`I.seq(PP.lazy(() => 'foo'), PP.render(10))`, 'foo')
-
-  testEq(`I.seq('foo', PP.enclose(PP.dquotes), PP.render(10))`, '"foo"')
-
-  testEq(
-    `I.seq(['foo', 'bar', 'baz'],
-           PP.punctuate(','),
-           PP.intersperse(PP.line),
-           PP.render(10))`,
-    'foo,\nbar,\nbaz'
+  testEq('foo-bar', () =>
+    I.seq(PP.intersperse('-', ['foo', 'bar']), PP.render(10))
   )
 
-  testEq(`I.seq(PP.choice('wide', 'narrow'), PP.render(10))`, 'wide')
+  testEq('foo', () => I.seq(PP.lazy(() => 'foo'), PP.render(10)))
 
-  testEq(`I.seq(PP.choice('wide', 'narrow'), PP.render(2))`, 'narrow')
+  testEq('"foo"', () => I.seq('foo', PP.enclose(PP.dquotes), PP.render(10)))
 
-  testEq(
-    `I.seq(PP.intersperse(PP.line, ['foo', 'bar']),
-           PP.group,
-           PP.render(10))`,
-    'foo bar'
+  testEq('foo,\nbar,\nbaz', () =>
+    I.seq(
+      ['foo', 'bar', 'baz'],
+      PP.punctuate(','),
+      PP.intersperse(PP.line),
+      PP.render(10)
+    )
   )
 
-  testEq(
-    `I.seq(PP.intersperse(PP.line, ['foo', 'bar']),
-           PP.nest('--'),
-           PP.render(10))`,
-    'foo\n--bar'
+  testEq('wide', () => I.seq(PP.choice('wide', 'narrow'), PP.render(10)))
+
+  testEq('narrow', () => I.seq(PP.choice('wide', 'narrow'), PP.render(2)))
+
+  testEq('foo bar', () =>
+    I.seq(PP.intersperse(PP.line, ['foo', 'bar']), PP.group, PP.render(10))
   )
 
-  testEq(
-    `I.seq(PP.intersperse(PP.line, ['foo', 'bar']),
-           PP.nest(2),
-           PP.render(10))`,
-    'foo\n  bar'
+  testEq('foo\n--bar', () =>
+    I.seq(PP.intersperse(PP.line, ['foo', 'bar']), PP.nest('--'), PP.render(10))
   )
 
-  testEq(
-    `I.seq(PP.intersperse(PP.line, ['foo', 'bar']),
-           PP.nest(2),
-           PP.group,
-           PP.render(10))`,
-    'foo bar'
+  testEq('foo\n  bar', () =>
+    I.seq(PP.intersperse(PP.line, ['foo', 'bar']), PP.nest(2), PP.render(10))
   )
 
-  testEq(
-    `I.seq(PP.intersperse(PP.lineBreak, [PP.lazy(() => 'foo'), 'bar']),
-           PP.group,
-           PP.group,
-           PP.nest(2),
-           PP.render(10))`,
-    'foobar'
+  testEq('foo bar', () =>
+    I.seq(
+      PP.intersperse(PP.line, ['foo', 'bar']),
+      PP.nest(2),
+      PP.group,
+      PP.render(10)
+    )
   )
 
-  testEq(
-    `I.seq(PP.lazy(() => I.seq(
-             ['a', 'b', 'c'],
-             PP.intersperse(PP.line),
-             PP.indent('// '),
-             PP.prepend(' '))),
-           PP.group,
-           PP.render(5))`,
-    ' // a\n // b\n // c'
+  testEq('foobar', () =>
+    I.seq(
+      PP.intersperse(PP.lineBreak, [PP.lazy(() => 'foo'), 'bar']),
+      PP.group,
+      PP.group,
+      PP.nest(2),
+      PP.render(10)
+    )
   )
 
-  testEq(
-    `I.seq(PP.lazy(() => I.seq(
-             ['a', 'b', 'c'],
-             PP.intersperse(PP.line),
-             PP.indent('// '),
-             PP.prepend(' '))),
-           PP.group,
-           PP.render(15))`,
-    ' // a b c'
+  testEq(' // a\n // b\n // c', () =>
+    I.seq(
+      PP.lazy(() =>
+        I.seq(
+          ['a', 'b', 'c'],
+          PP.intersperse(PP.line),
+          PP.indent('// '),
+          PP.prepend(' ')
+        )
+      ),
+      PP.group,
+      PP.render(5)
+    )
   )
 
-  testEq(
-    `I.seq(['column: ', PP.column(c => c.toString())],
-           PP.group,
-           PP.render(9))`,
-    'column: 8'
+  testEq(' // a b c', () =>
+    I.seq(
+      PP.lazy(() =>
+        I.seq(
+          ['a', 'b', 'c'],
+          PP.intersperse(PP.line),
+          PP.indent('// '),
+          PP.prepend(' ')
+        )
+      ),
+      PP.group,
+      PP.render(15)
+    )
   )
 
-  testEq(
-    `I.seq(['nesting:', PP.line, PP.nesting(c => c.toString())],
-           PP.group,
-           PP.nest(2),
-           PP.render(9))`,
-    'nesting:\n  2'
+  testEq('column: 8', () =>
+    I.seq(['column: ', PP.column(c => c.toString())], PP.group, PP.render(9))
   )
 
-  testEq(
-    `{ const d = PP.lazy(() => 'foo')
-     ; return PP.render(10, PP.group([d, PP.render(10, d)]))
-     }`,
-    'foofoo'
+  testEq('nesting:\n  2', () =>
+    I.seq(
+      ['nesting:', PP.line, PP.nesting(c => c.toString())],
+      PP.group,
+      PP.nest(2),
+      PP.render(9)
+    )
   )
 
-  testEq(`PP.render(10, PP.group([]))`, '')
+  testEq('foofoo', () => {
+    const d = PP.lazy(() => 'foo')
+    return PP.render(10, PP.group([d, PP.render(10, d)]))
+  })
+
+  testEq('', () => PP.render(10, PP.group([])))
 })
