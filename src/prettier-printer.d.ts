27dae7e920b91f3e4f45a0716dddfadeef052222
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-06-19 21:57:48 +0300

    Add overloaded typings for curried functions (except `renderWith`)

--- src/prettier-printer.d.ts
+++ src/prettier-printer.d.ts
@@ -4,9 +4,11 @@ declare module 'prettier-printer' {
 
   // Rendering documents
   function render(maxCols: number, doc: IDoc): string
+  function render(maxCols: number): (doc: IDoc) => string
+
   function renderWith<State>(
     actions: {
-      text: (state: State, span: string) => State,
+      text: (state: State, span: string) => State
       line: (state: State) => State
     },
     state: State,
@@ -22,17 +24,25 @@ declare module 'prettier-printer' {
 
   // Concatenating documents
   function append(rhsDoc: IDoc, lhsDoc: IDoc): IDoc
+  function append(rhsDoc: IDoc): (lhsDoc: IDoc) => IDoc
+
   function prepend(lhsDoc: IDoc, rhsDoc: IDoc): IDoc
+  function prepend(lhsDoc: IDoc): (rhsDoc: IDoc) => IDoc
 
   // Lists of documents
   function intersperse(doc: IDoc, docs: IDoc[]): IDoc[]
+  function intersperse(doc: IDoc): (docs: IDoc[]) => IDoc[]
+
   function punctuate(sepDoc: IDoc, docs: IDoc[]): IDoc[]
+  function punctuate(sepDoc: IDoc): (docs: IDoc[]) => IDoc[]
 
   // Lazy documents
   function lazy(f: () => IDoc): IDoc
 
   // Enclosing documents
   function enclose(docPair: [IDoc, IDoc], doc: IDoc): IDoc
+  function enclose(docPair: [IDoc, IDoc]): (doc: IDoc) => IDoc
+
   const angles: [IDoc, IDoc]
   const braces: [IDoc, IDoc]
   const brackets: [IDoc, IDoc]
@@ -45,10 +55,13 @@ declare module 'prettier-printer' {
 
   // Alternative documents
   function choice(wideDoc: IDoc, narrowDoc: IDoc): IDoc
+  function choice(wideDoc: IDoc): (narrowDoc: IDoc) => IDoc
+
   function group(doc: IDoc): IDoc
 
   // Nested documents
   function nest(indent: string | number, doc: IDoc): IDoc
+  function nest(indent: string | number): (doc: IDoc) => IDoc
 
   // Layout dependent documents
   function column(f: (column: number) => IDoc): IDoc
@@ -56,6 +69,10 @@ declare module 'prettier-printer' {
 
   // Aligned documents
   function align(doc: IDoc): IDoc
+
   function hang(indent: string | number, doc: IDoc): IDoc
+  function hang(indent: string | number): (doc: IDoc) => IDoc
+
   function indent(indent: string | number, doc: IDoc): IDoc
+  function indent(indent: string | number): (doc: IDoc) => IDoc
 }
